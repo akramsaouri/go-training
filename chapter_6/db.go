809c3d218165d95778e04edd8c73e392c56e97c4
Akram Saouri <akram@hiddenfounders.com>
2016-10-24 18:07:03 +0100

    console-io

--- chapter_6/db.go
+++ chapter_6/db.go
@@ -25,25 +25,50 @@ func main() {
 	c := session.DB("go-training").C("biblio")
 
 	deleteAll(c)
-	create(c)
-	show(c)
-	delete(c)
-	show(c)
-	update(c)
-	show(c)
+
+	choice := -1
+	for choice != 0 {
+		fmt.Println("1: create")
+		fmt.Println("2: read")
+		fmt.Println("3: update")
+		fmt.Println("4: delete")
+		fmt.Println("0: quit")
+		fmt.Scanf("%d", &choice)
+		switch choice {
+		case 1:
+			fmt.Println("Creating new book:")
+			var title, writer string
+			fmt.Println("> title: ")
+			fmt.Scanf("%s", &title)
+			fmt.Println("> writer: ")
+			fmt.Scanf("%s", &writer)
+			create(c, Book{title, writer, time.Now()})
+		case 2:
+			fmt.Println("Reading all books:")
+			show(c)
+		case 3:
+			fmt.Println("Updating a book:")
+			var title, newTitle, newWriter string
+			fmt.Println("> title:")
+			fmt.Scanf("%s", &title)
+			fmt.Println("> new title:")
+			fmt.Scanf("%s", &newTitle)
+			fmt.Println("> new writer:")
+			fmt.Scanf("%s", &newWriter)
+			update(c, title, Book{newTitle, newWriter, time.Now()})
+		case 4:
+			fmt.Println("Deleting a book:")
+			var title string
+			fmt.Println("> title:")
+			fmt.Scanf("%s", &title)
+			delete(c, title)
+		}
+		fmt.Println("----------------")
+	}
 }
 
-func create(c *mgo.Collection) {
-	var title, writer string
-
-	fmt.Println("> title: ")
-	fmt.Scanf("%s", &title)
-	fmt.Println("> writer: ")
-	fmt.Scanf("%s", &writer)
-
-	err := c.Insert(
-		&Book{title, writer, time.Now()},
-	)
+func create(c *mgo.Collection, book Book) {
+	err := c.Insert(book)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -58,10 +83,8 @@ func read(c *mgo.Collection) (results []Book) {
 	return
 }
 
-func delete(c *mgo.Collection) {
-	var title string
-	fmt.Println("> title:")
-	fmt.Scanf("%s", &title)
+func delete(c *mgo.Collection, title string) {
+
 	err := c.Remove(bson.M{"title": title})
 	if err != nil {
 		fmt.Println("Book not found.")
@@ -70,16 +93,7 @@ func delete(c *mgo.Collection) {
 	}
 }
 
-func update(c *mgo.Collection) {
-	var title, newTitle, newWriter string
-	fmt.Println("> title:")
-	fmt.Scanf("%s", &title)
-	fmt.Println("> new title:")
-	fmt.Scanf("%s", &newTitle)
-	fmt.Println("> new writer:")
-	fmt.Scanf("%s", &newWriter)
-
-	book := Book{newTitle, newWriter, time.Now()}
+func update(c *mgo.Collection, title string, book Book) {
 	err := c.Update(bson.M{"title": title}, book)
 	if err != nil {
 		fmt.Println("Book not found")
@@ -89,12 +103,9 @@ func update(c *mgo.Collection) {
 }
 
 func show(c *mgo.Collection) {
-	fmt.Println("--------------------")
 	for _, result := range read(c) {
-		// TODO: add toString method
 		fmt.Println(result)
 	}
-	fmt.Println("--------------------")
 }
 
 func deleteAll(c *mgo.Collection) {
@@ -104,10 +115,9 @@ func deleteAll(c *mgo.Collection) {
 	}
 }
 
-//TODO: user enter book details via console
-//TODO: console io
 //TODO: add toString method to book
 //TODO: bind crud method to Book model
 //TODO: make title unique
 //TODO: make the read method accept an optional query
 //TODO : PublishedAt bug
+// TODO: add toString method
