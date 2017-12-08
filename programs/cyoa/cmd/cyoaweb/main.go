396c4937bd0124f5976d36fe3bfae633e2749100
akramsaouri <akram@hiddenfounders.com>
2017-12-08 22:33:11 +0000

    choose your own adventue

---
+++ programs/cyoa/cmd/cyoaweb/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/akramsaouri/gophercises/programs/cyoa"
+)
+
+func main() {
+	port := flag.Int("port", 3000, "the port to start the CYOA web application on")
+	filename := flag.String("filename", "gopher.json", "the JSON filename with the CYOA story")
+	flag.Parse()
+	fmt.Printf("Using the strory in %s.\n", *filename)
+
+	f, err := os.Open(*filename)
+
+	if err != nil {
+		panic(err)
+	}
+
+	story, err := cyoa.JsonStory(f)
+	if err != nil {
+		panic(err)
+	}
+
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Starting the server on port %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
+}
